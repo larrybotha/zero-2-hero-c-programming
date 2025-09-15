59169d42d4a0985f5b600a09a436cc6709f1eb17
Larry Botha <lawrenceguy@gmail.com>
2025-09-15 09:19:13 +0200

    feat: add struct example

---
+++ src/06-struct.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#define MAX_EMPLOYEES 1000
+
+struct employee_t {
+  int id;
+  char firstname[64];
+  char lastname[64];
+  float income;
+  bool ismanager;
+};
+
+int main() {
+  struct employee_t employees[(int)MAX_EMPLOYEES];
+
+  int i = 0;
+
+  for (i = 0; i < MAX_EMPLOYEES; i++) {
+    employees[i].income = 0;
+    employees[i].ismanager = false;
+  }
+
+  printf("%f\n", employees[9].income);
+}
