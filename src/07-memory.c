4b082db933e0b95b55ea9e30f1b1c8cc2af3dcdc
Larry Botha <lawrenceguy@gmail.com>
2025-09-17 23:41:02 +0200

    feat(memory): add example of evaling memory

---
+++ src/07-memory.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int *ptr = (int *)malloc(sizeof(int));
+
+	if (ptr == NULL) {
+		printf("Memory allocation failed\\n");
+        	
+		return 1;
+	}
+
+	*ptr = 42;
+
+	printf(
+			"value: %d, address: %p\\n",
+			*ptr,
+			(void*)ptr
+	);
+	free(ptr)
+
+	return 0;
+}
